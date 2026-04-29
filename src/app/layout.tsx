443c4cadaebcd83c8b1a45541a5283bf14683d5e
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-04-29 10:12:29 +0600

    shadcn fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import { store } from "@/redux/store";
 import { Provider } from "react-redux";
 import { AuthProvider } from "@/components/shared/AuthProvider";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <Provider store={store}>
           <AuthProvider>{children}</AuthProvider>
